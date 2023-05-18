550902cc7ddbd7f681b2cff3ab0c896c3497971c
Taehyung Kwon <katalogin88@gmail.com>
2023-05-18 10:55:28 +0100

    Modify structure using html

--- main.js
+++ main.js
@@ -7,55 +7,7 @@ const createWindow = () => {
     height: 600,
   });
 
-  const progressBar = new ProgressBar({
-    text: "Loading...",
-    detail: "Please wait",
-    browserWindow: {
-      parent: win,
-      modal: true,
-      closable: false,
-      minimizable: false,
-      width: 400,
-      height: 100,
-    },
-    style: {
-      text: {},
-      detail: {},
-      bar: {
-        background: "#e0e0e0",
-        height: "10px",
-        width: "0%",
-      },
-      value: {},
-    },
-    maxValue: 100, // Maximum value of the progress bar
-  });
-
-  progressBar
-    .on("completed", () => {
-      console.log("Loading completed.");
-      progressBar.detail = "Completed.";
-      setTimeout(() => {
-        progressBar.close();
-      }, 1000);
-    })
-    .on("aborted", () => {
-      console.log("Loading aborted.");
-      progressBar.detail = "Aborted.";
-      setTimeout(() => {
-        progressBar.close();
-      }, 1000);
-    });
-
-  win.loadURL("https://parrotias.com");
-
-  win.webContents.on("did-finish-load", () => {
-    progressBar.setCompleted();
-  });
-
-  win.webContents.on("did-fail-load", () => {
-    progressBar.setAborted();
-  });
+  win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
