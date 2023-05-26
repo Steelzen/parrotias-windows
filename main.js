c892e2369f5fc23ca41882a0812575a19455b2b7
Taehyung Kwon <katalogin88@gmail.com>
2023-05-26 02:17:03 +0100

    Add print menu into context menu

--- main.js
+++ main.js
@@ -1,4 +1,70 @@
-const { app, BrowserWindow } = require("electron");
+// const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+// const { handleMenu } = require("./scripts/menu.js");
+// const {
+//   rendererToMainAPI,
+//   mainToRendererAPI,
+// } = require("./scripts/electron-api.js");
+// // require('update-electron-app')()
+
+// const createWindow = () => {
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       preload: path.join(__dirname, "preload.js"),
+//     },
+//   });
+
+//   const interfaceWindow = new BrowserWindow({
+//     width: 800,
+//     height: 100,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       preload: path.join(__dirname, "preload.js"),
+//     },
+//   });
+
+//   mainWindow.loadURL("https://parrotias.com");
+//   interfaceWindow.loadFile("index.html");
+
+//   rendererToMainAPI(mainWindow);
+//   handleMenu(mainWindow);
+
+//   // for sending a message to renderer process
+//   mainToRendererAPI(mainWindow.webContents, interfaceWindow.webContents);
+
+//   // mainWindow.webContents.on("did-finish-load", () => {
+//   //   // Trigger the print action
+//   //   mainWindow.webContents.print();
+//   // });
+// };
+
+// app
+//   .whenReady()
+//   .then(() => {
+//     createWindow();
+
+//     app.on("activate", () => {
+//       if (BrowserWindow.getAllWindows().length === 0) {
+//         createWindow();
+//       }
+//     });
+//   })
+//   .catch((error) => {
+//     // Handle any promise rejections here
+//     console.error("Error:", error);
+//   });
+
+// app.on("window-all-closed", () => {
+//   if (process.platform !== "darwin") app.quit();
+// });
+
+// app.setAppUserModelId(process.execPath);
+
+// TEst for view//
+const { app, BrowserWindow, BrowserView } = require("electron");
 const path = require("path");
 const { handleMenu } = require("./scripts/menu.js");
 const {
@@ -17,28 +83,25 @@ const createWindow = () => {
     },
   });
 
-  const interfaceWindow = new BrowserWindow({
-    width: 800,
-    height: 100,
+  const view = new BrowserView({
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
+  mainWindow.setBrowserView(view);
+  view.setBounds({ x: 0, y: 28, width: 800, height: 40 });
+  view.setAutoResize({ width: true, height: true });
+
   mainWindow.loadURL("https://parrotias.com");
-  interfaceWindow.loadFile("index.html");
+  view.webContents.loadURL(`file://${path.join(__dirname, "index.html")}`);
 
   rendererToMainAPI(mainWindow);
   handleMenu(mainWindow);
 
   // for sending a message to renderer process
-  mainToRendererAPI(mainWindow.webContents, interfaceWindow.webContents);
-
-  // mainWindow.webContents.on("did-finish-load", () => {
-  //   // Trigger the print action
-  //   mainWindow.webContents.print();
-  // });
+  mainToRendererAPI(mainWindow.webContents, view.webContents);
 };
 
 app
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,101 +1,106 @@
 const { Menu, MenuItem } = require("electron");
-const contextMenu = require('electron-context-menu'); 
+const contextMenu = require("electron-context-menu");
 
 const handleMenu = (mainWindow) => {
-    setContextMenu();
-    disableMenuBarVisbility(mainWindow);
-    createMenu(mainWindow);
-}
+  setContextMenu(mainWindow);
+  disableMenuBarVisbility(mainWindow);
+  createMenu(mainWindow);
+};
 
-const setContextMenu = () => {
-    // Display SaveImageAs function when right click to any image item
-    contextMenu({
-        showSaveImageAs: true
-    })
-}
-
-const disableMenuBarVisbility = (mainWindow) =>{
-    mainWindow.setMenuBarVisibility(false);
-}
-  
-const createMenu = (mainWindow) =>{
-    const isMac = process.platform === 'darwin'
-    const printMenuItem = new MenuItem({
-        label: 'Print',
-        accelerator: 'CmdOrCtrl+P',
-        click: () => {
+const setContextMenu = (mainWindow) => {
+  // Display SaveImageAs function when right click to any image item
+  contextMenu({
+    showSaveImageAs: true,
+    prepend: () => {
+      return [
+        {
+          label: "Print",
+          click: () => {
             mainWindow.webContents.print();
+          },
         },
-    });
+      ];
+    },
+  });
+};
 
-    const template = [
-        // { role: 'editMenu' }
-        {
-        label: 'Edit',
-        submenu: [
-            { role: 'undo' },
-            { role: 'redo' },
-            { type: 'separator' },
-            { role: 'cut' },
-            { role: 'copy' },
-            { role: 'paste' },
-            ...(isMac ? [
-            { role: 'pasteAndMatchStyle' },
-            { role: 'delete' },
-            { role: 'selectAll' },
-            { type: 'separator' },
-            {
-                label: 'Speech',
-                submenu: [
-                { role: 'startSpeaking' },
-                { role: 'stopSpeaking' }
-                ]
-            }
-            ] : [
-            { role: 'delete' },
-            { type: 'separator' },
-            { role: 'selectAll' }
-            ])
-        ]
-        },
-        // { role: 'viewMenu' }
-        {
-        label: 'View',
-        submenu: [
-            { role: 'reload', accelerator: 'F5' },
-            { role: 'forceReload' },
-            { role: 'toggleDevTools' },
-            { type: 'separator' },
-            { role: 'resetZoom' },
-            { role: 'zoomIn' },
-            { role: 'zoomOut' },
-            { type: 'separator' },
-            { role: 'togglefullscreen' }
-        ]
-        },
-        // { role: 'windowMenu' }
-        {
-        label: 'Window',
-        submenu: [
-            { role: 'minimize' },
-            { role: 'zoom' },
-            printMenuItem,
-            ...(isMac ? [
-            { type: 'separator' },
-            { role: 'front' },
-            { type: 'separator' },
-            { role: 'window' }
-            ] : [
-            { role: 'close' }
-            ])
-        ]
-        },
-    ]
+const disableMenuBarVisbility = (mainWindow) => {
+  mainWindow.setMenuBarVisibility(false);
+};
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-}
+const createMenu = (mainWindow) => {
+  const isMac = process.platform === "darwin";
+  const printMenuItem = new MenuItem({
+    label: "Print",
+    accelerator: "CmdOrCtrl+P",
+    click: () => {
+      mainWindow.webContents.print();
+    },
+  });
+
+  const template = [
+    // { role: 'editMenu' }
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              },
+            ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+      ],
+    },
+    // { role: 'viewMenu' }
+    {
+      label: "View",
+      submenu: [
+        { role: "reload", accelerator: "F5" },
+        { role: "forceReload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+    // { role: 'windowMenu' }
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        printMenuItem,
+        ...(isMac
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" },
+            ]
+          : [{ role: "close" }]),
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+};
 
 module.exports = {
-    handleMenu,
-}
+  handleMenu,
+};
