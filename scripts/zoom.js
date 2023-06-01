b82f759bcc53954f7f12cc29113891dee4b0790e
Steelzen <katalogin88@gmail.com>
2023-06-01 09:16:27 +0900

    Solve double 0.0 error in zoom function

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -3,26 +3,23 @@ const zoom = (websiteView) => {
 
   win.webContents.setZoomFactor(1.0);
 
-  // Upper Limit is working of 500 %
+  // Upper Limit is working of 500%
   win.webContents
     .setVisualZoomLevelLimits(1, 5)
-    .then(console.log("Zoom Levels Have been Set between 100% and 500%"))
+    .then(() => console.log("Zoom levels have been set between 100% and 500%"))
     .catch((err) => console.log(err));
 
-  try {
-    win.webContents.on("zoom-changed", (event, zoomDirection) => {
-      var currentZoom = win.webContents.getZoomFactor();
+  win.webContents.on("zoom-changed", (event, zoomDirection) => {
+    const currentZoom = win.webContents.getZoomFactor();
 
-      if (zoomDirection === "in") {
-        win.webContents.zoomFactor = currentZoom + 0.2;
-      }
-      if (zoomDirection === "out") {
-        win.webContents.zoomFactor = currentZoom - 0.2;
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    if (zoomDirection === "in") {
+      win.webContents.zoomFactor = Math.min(currentZoom + 0.2, 5);
+    }
+
+    if (zoomDirection === "out") {
+      win.webContents.zoomFactor = Math.max(currentZoom - 0.2, 0.2);
+    }
+  });
 };
 
 module.exports = {
